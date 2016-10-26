395a98526df679f8acfa42608d4854f3576cc462
ishibashi <ishi@moremost.jp>
2016-10-26 09:50:55 +0900

    migration作成中。

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -5,30 +5,40 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::drop('users');
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function (Blueprint $table) {
+			$table->increments('id');
+			$table->integer('role')->default(2);
+			$table->integer('status');
+			$table->integer('department')->default(0);
+			$table->string('last_name');
+			$table->string('first_name');
+			$table->string('email')->unique();
+			$table->string('password', 60);
+			$table->date('date_of_entering');
+			$table->date('base_date');
+			$table->text('memo')->nullable()->default(NULL);
+			$table->integer('retire_flg')->nullable()->default(NULL);
+			$table->rememberToken();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
 }
