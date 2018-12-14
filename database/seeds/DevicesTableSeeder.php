d2d03afe6a9d67bf7f23fc477c58b15a07496db9
yasuken1990 <yasuken1990@gmail.com>
2018-12-14 17:08:42 +0900

    [fix] seedを実行するとtruncateしてしまうので、truncateしないように修正

--- database/seeds/DevicesTableSeeder.php
+++ database/seeds/DevicesTableSeeder.php
@@ -1,95 +1,97 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Device;
 
 class DevicesTableSeeder extends Seeder
 {
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		DB::table('devices')->truncate();
-		DB::table('devices')->insert([
-			[
-				'category' => 1,
-				'os' => 1,
-				'name' => '江藤さんMacBookAir',
-				'serial_id' => '',
-				'bought_at' => '2013-04-01',
-				'user_id' => 1,
-				'status' => 1,
-				'core' => 4,
-				'memory' => 8,
-				'capacity' => 500,
-				'size' => 13,
-				'created_at' => Carbon\Carbon::now(),
-				'updated_at' => Carbon\Carbon::now(),
-			],
-			[
-				'category' => 1,
-				'os' => 1,
-				'name' => '佐藤さんMacBookPro',
-				'serial_id' => '',
-				'bought_at' => '2016-07-01',
-				'user_id' => 2,
-				'status' => 1,
-				'core' => 8,
-				'memory' => 16,
-				'capacity' => 1000,
-				'size' => 15,
-				'created_at' => Carbon\Carbon::now(),
-				'updated_at' => Carbon\Carbon::now(),
-			],
-			[
-				'category' => 1,
-				'os' => 1,
-				'name' => 'ちっさいMacBookAir',
-				'serial_id' => '123456789',
-				'bought_at' => '2013-10-01',
-				'user_id' => 3,
-				'status' => 1,
-				'core' => 4,
-				'memory' => 8,
-				'capacity' => 500,
-				'size' => 11,
-				'created_at' => Carbon\Carbon::now(),
-				'updated_at' => Carbon\Carbon::now(),
-			],
-			[
-				'category' => 40,
-				'os' => null,
-				'name' => 'BenQの24インチディスプレイ',
-				'serial_id' => '',
-				'bought_at' => '2016-02-01',
-				'user_id' => 3,
-				'status' => 1,
-				'core' => null,
-				'memory' => null,
-				'capacity' => null,
-				'size' => 24,
-				'created_at' => Carbon\Carbon::now(),
-				'updated_at' => Carbon\Carbon::now(),
-			],
-			[
-				'category' => 2,
-				'os' => 1,
-				'name' => '総務用デスクトップMacPC',
-				'serial_id' => '',
-				'bought_at' => '2014-04-01',
-				'user_id' => 4,
-				'status' => 1,
-				'core' => 4,
-				'memory' => 8,
-				'capacity' => 500,
-				'size' => 24,
-				'created_at' => Carbon\Carbon::now(),
-				'updated_at' => Carbon\Carbon::now(),
-			],
-		]);
-	}
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if (count(DB::table('devices')->get()) === 0) {
+            DB::table('devices')->insert([
+                [
+                    'category' => 1,
+                    'os' => 1,
+                    'name' => '江藤さんMacBookAir',
+                    'serial_id' => '',
+                    'bought_at' => '2013-04-01',
+                    'user_id' => 1,
+                    'status' => 1,
+                    'core' => 4,
+                    'memory' => 8,
+                    'capacity' => 500,
+                    'size' => 13,
+                    'created_at' => Carbon\Carbon::now(),
+                    'updated_at' => Carbon\Carbon::now(),
+                ],
+                [
+                    'category' => 1,
+                    'os' => 1,
+                    'name' => '佐藤さんMacBookPro',
+                    'serial_id' => '',
+                    'bought_at' => '2016-07-01',
+                    'user_id' => 2,
+                    'status' => 1,
+                    'core' => 8,
+                    'memory' => 16,
+                    'capacity' => 1000,
+                    'size' => 15,
+                    'created_at' => Carbon\Carbon::now(),
+                    'updated_at' => Carbon\Carbon::now(),
+                ],
+                [
+                    'category' => 1,
+                    'os' => 1,
+                    'name' => 'ちっさいMacBookAir',
+                    'serial_id' => '123456789',
+                    'bought_at' => '2013-10-01',
+                    'user_id' => 3,
+                    'status' => 1,
+                    'core' => 4,
+                    'memory' => 8,
+                    'capacity' => 500,
+                    'size' => 11,
+                    'created_at' => Carbon\Carbon::now(),
+                    'updated_at' => Carbon\Carbon::now(),
+                ],
+                [
+                    'category' => 40,
+                    'os' => null,
+                    'name' => 'BenQの24インチディスプレイ',
+                    'serial_id' => '',
+                    'bought_at' => '2016-02-01',
+                    'user_id' => 3,
+                    'status' => 1,
+                    'core' => null,
+                    'memory' => null,
+                    'capacity' => null,
+                    'size' => 24,
+                    'created_at' => Carbon\Carbon::now(),
+                    'updated_at' => Carbon\Carbon::now(),
+                ],
+                [
+                    'category' => 2,
+                    'os' => 1,
+                    'name' => '総務用デスクトップMacPC',
+                    'serial_id' => '',
+                    'bought_at' => '2014-04-01',
+                    'user_id' => 4,
+                    'status' => 1,
+                    'core' => 4,
+                    'memory' => 8,
+                    'capacity' => 500,
+                    'size' => 24,
+                    'created_at' => Carbon\Carbon::now(),
+                    'updated_at' => Carbon\Carbon::now(),
+                ],
+            ]);
+        }
+    }
 
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -5,80 +5,81 @@ use Illuminate\Database\Seeder;
 class UsersTableSeeder extends Seeder
 {
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		DB::table('users')->truncate();
-		DB::table('users')->insert([
-			[
-				'id' => 1,
-				'role' => 1,
-				'status' => 1,
-				'type_of_employment' => 50,
-				'department' => 1,
-				'last_name' => '江藤',
-				'first_name' => '勝彦',
-				'email' => 'katsu@moremost.jp',
-				'password' => bcrypt('aaaaaaaa'),
-				'date_of_entering' => '2013-04-01',
-				'base_date' => '2013-010-01',
-				'memo' => '',
-				'created_at' => Carbon\Carbon::now(),
-				'updated_at' => Carbon\Carbon::now(),
-			],
-			[
-				'id' => 2,
-				'role' => 1,
-				'status' => 1,
-				'type_of_employment' => 50,
-				'department' => 1,
-				'last_name' => '佐藤',
-				'first_name' => '裕也',
-				'email' => 'sato@moremost.jp',
-				'password' => bcrypt('aaaaaaaa'),
-				'date_of_entering' => '2014-04-01',
-				'base_date' => '2014-010-01',
-				'memo' => '',
-				'created_at' => Carbon\Carbon::now(),
-				'updated_at' => Carbon\Carbon::now(),
-			],
-			[
-				'id' => 3,
-				'role' => 1,
-				'status' => 1,
-				'type_of_employment' => 1,
-				'department' => 1,
-				'last_name' => '石橋',
-				'first_name' => '美内子',
-				'email' => 'ishi@moremost.jp',
-				'password' => bcrypt('aaaaaaaa'),
-				'date_of_entering' => '2015-08-03',
-				'base_date' => '2016-02-03',
-				'memo' => '',
-				'created_at' => Carbon\Carbon::now(),
-				'updated_at' => Carbon\Carbon::now(),
-			],
-			[
-				'id' => 4,
-				'role' => 1,
-				'status' => 1,
-				'type_of_employment' => 1,
-				'department' => 50,
-				'last_name' => '生田',
-				'first_name' => '弥佳',
-				'email' => 'mika@moremost.jp',
-				'password' => bcrypt('aaaaaaaa'),
-				'date_of_entering' => '2016-08-01',
-				'base_date' => '2017-02-01',
-				'memo' => '',
-				'created_at' => Carbon\Carbon::now(),
-				'updated_at' => Carbon\Carbon::now(),
-			],
-		]);
-	}
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if (count(DB::table('devices')->get()) === 0) {
+            DB::table('users')->insert([
+                [
+                    'id' => 1,
+                    'role' => 1,
+                    'status' => 1,
+                    'type_of_employment' => 50,
+                    'department' => 1,
+                    'last_name' => '江藤',
+                    'first_name' => '勝彦',
+                    'email' => 'katsu@moremost.jp',
+                    'password' => bcrypt('aaaaaaaa'),
+                    'date_of_entering' => '2013-04-01',
+                    'base_date' => '2013-010-01',
+                    'memo' => '',
+                    'created_at' => Carbon\Carbon::now(),
+                    'updated_at' => Carbon\Carbon::now(),
+                ],
+                [
+                    'id' => 2,
+                    'role' => 1,
+                    'status' => 1,
+                    'type_of_employment' => 50,
+                    'department' => 1,
+                    'last_name' => '佐藤',
+                    'first_name' => '裕也',
+                    'email' => 'sato@moremost.jp',
+                    'password' => bcrypt('aaaaaaaa'),
+                    'date_of_entering' => '2014-04-01',
+                    'base_date' => '2014-010-01',
+                    'memo' => '',
+                    'created_at' => Carbon\Carbon::now(),
+                    'updated_at' => Carbon\Carbon::now(),
+                ],
+                [
+                    'id' => 3,
+                    'role' => 1,
+                    'status' => 1,
+                    'type_of_employment' => 1,
+                    'department' => 1,
+                    'last_name' => '石橋',
+                    'first_name' => '美内子',
+                    'email' => 'ishi@moremost.jp',
+                    'password' => bcrypt('aaaaaaaa'),
+                    'date_of_entering' => '2015-08-03',
+                    'base_date' => '2016-02-03',
+                    'memo' => '',
+                    'created_at' => Carbon\Carbon::now(),
+                    'updated_at' => Carbon\Carbon::now(),
+                ],
+                [
+                    'id' => 4,
+                    'role' => 1,
+                    'status' => 1,
+                    'type_of_employment' => 1,
+                    'department' => 50,
+                    'last_name' => '生田',
+                    'first_name' => '弥佳',
+                    'email' => 'mika@moremost.jp',
+                    'password' => bcrypt('aaaaaaaa'),
+                    'date_of_entering' => '2016-08-01',
+                    'base_date' => '2017-02-01',
+                    'memo' => '',
+                    'created_at' => Carbon\Carbon::now(),
+                    'updated_at' => Carbon\Carbon::now(),
+                ],
+            ]);
+        }
+    }
 
 }
